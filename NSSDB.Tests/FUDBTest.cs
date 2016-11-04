8ca7ceb2816ae5e028699dc7d38befbc947dd0b3
jknewson <jknewson@usgs.gov>
2016-11-04 11:04:44 -0600

    db update and minor changes to to NSSAgent

---
+++ NSSDB.Tests/FUDBTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FU_NSSDB;
+using System.Linq;
+using NSSDB;
+
+namespace NSSDB.Tests
+{
+    [TestClass]
+    public class FUDBTest
+    {
+        [TestMethod]
+        public void ForceUpdate()
+        {
+            //var x = new ForceUpdate();
+            //x.Load();
+                Assert.IsTrue(true);
+            
+        }
+    }
+
+
+
+}

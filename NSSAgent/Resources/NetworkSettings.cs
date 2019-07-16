17a95ab679c33e73c5b4609da969a181a5ebc064
Jeremy Newson <jknewson@usgs.gov>
2019-07-16 14:33:33 -0600

    enabled QPPQ x 2

---
+++ NSSAgent/Resources/NetworkSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WIM.Utilities.Resources;
+
+namespace NSSAgent.Resources
+{
+    public class NetworkSettings
+    {
+        public Resource NWIS { get; set; }       
+    }
+}

da503fa7db2b7fabba459171a645a35e03da20a2
Wavra <hwavra@usgs.gov>
2021-08-16 12:00:26 -0500

    working on #192

--- SharedDB/Migrations/20210813142650_addUnitTypeStatGroups.Designer.cs
+++ SharedDB/Migrations/20210813142650_addUnitTypeStatGroups.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SharedDB;
@@ -9,9 +10,10 @@ using SharedDB;
 namespace SharedDB.Migrations
 {
     [DbContext(typeof(SharedDBContext))]
-    partial class SharedDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210813142650_addUnitTypeStatGroups")]
+    partial class addUnitTypeStatGroups
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
